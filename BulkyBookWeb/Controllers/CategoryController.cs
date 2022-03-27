8f4948876bc889feb65271ab4e18680f50edecdc
Si Lam <silam@hotmail.com>
2022-03-27 13:37:39 -0500

    Update records

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -48,5 +48,50 @@ namespace BulkyBookWeb.Controllers
             }
             return View(_category);
         }
+
+
+
+        // GET
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            //var categoryFromDb = _db.Categories.FirstOrDefault(c => c.Id == _id);
+            var categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public IActionResult Edit(Category _category)
+        {
+            if (_category.Name == _category.DisplayOrder.ToString())
+            {
+                //ModelState.AddModelError("CustomerError","DisplayOrder is not the same as Name");
+                ModelState.AddModelError("Name", "DisplayOrder is not the same as Name");
+
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(_category);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(_category);
+        }
     }
+
+
+
 }
